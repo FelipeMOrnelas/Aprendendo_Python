0af7d67dbf4a7a9f41687b73925b7de53f3f9bf8
Felipe M Ornelas <fe.mariuba@gmail.com>
2022-05-16 08:38:08 -0300

    Atualização
    Subindo Desafio_2 e correção no Desafio_1

--- PythonBasic/estudos_basicos/desafio_1.py
+++ PythonBasic/estudos_basicos/desafio_1.py
@@ -1,4 +1,4 @@
-# Desafio1
+# Desafio_1
 """
 Faça um programa que peça ao usuário para digitar um número inteiro, informe se este número é par ou ímpar.
 Caso o usuário não digite um número inteiro, informe que não é um número inteiro.
