0f279f92b15105cef9b332c55934c1d7e11f1c12
Felipe M Ornelas <fe.mariuba@gmail.com>
2022-05-20 12:56:23 -0300

    Atualização
    Subindo estudo 10 e desafio 5.
    
    No desafio 5 foi criado um validador de CPF!

--- PythonBasic/estudos_basicos/teste.py
+++ PythonBasic/estudos_basicos/teste.py
@@ -1,4 +1,39 @@
-n1 = input("Digite um numero: ")
+# n1 = input("Digite um numero: ")
+#
+# for i, quatidade in enumerate(n1 == 10):
+# #     print(i, quatidade)
+#
+# lista = [1234, 124132]
+# teste = ['','']
+# for i in range(0, 2):
+#     teste[i] = str(lista[i])
+#
+#
+# print(teste)
+#
+# teste = ''.join(teste)
+# print(teste)
+#
+# i = '12234455'
+# lista = ['', '', '', '', '', '', '', '']
+#
+# for teste, r in enumerate(i):
+#     lista[teste] = r
+# lista.pop()
+# lista.pop()
+#
+# print(lista)
+#
 
-for i, quatidade in enumerate(n1 == 10):
-    print(i, quatidade)
+# cpf = input("Digite seu CPF somente números: ")
+#
+# for n, r in enumerate(cpf):
+#     cpf_novo[n] = r
+#
+# cpf_novo.pop()
+# cpf_novo.pop()
+
+
+cpf = list(range(0,11))
+
+print(cpf)
