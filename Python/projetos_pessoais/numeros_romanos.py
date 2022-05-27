8d499988ad3c82056680c0f80ce40f530ffe4d86
Felipe M Ornelas <fe.mariuba@gmail.com>
2022-05-27 17:14:52 -0300

    Atualização
    Subindo o Projeto Pessoal de numero romanos para inteiros.
    
    Concluído com sucesso!!

--- Python/projetos_pessoais/numeros_romanos.py
+++ Python/projetos_pessoais/numeros_romanos.py
@@ -1,36 +1,80 @@
 def bug(romano):
-    lista = []
-    for n, i in enumerate(romano):
-        lista.append(i)
+    numero_romano = []
+    digitado = romano
+    i = 0
 
-    for i, n in enumerate(lista):
-        if lista[i] + lista[i+1] == 'VI':
-            continue
-        elif lista[i] + lista[i + 1] == 'IV':
-            lista.pop(i)
-            lista.pop(i)
-            lista.insert(i, 'IV')
-        elif lista[i] + lista[i + 1] == 'IX':
-            lista.pop(i)
-            lista.pop(i)
-            lista.insert(i, 'IX')
-        elif lista[i] + lista[i + 1] == 'XL':
-            lista.pop(i)
-            lista.pop(i)
-            lista.insert(i, 'XL')
-        elif lista[i] + lista[i + 1] == 'XC':
-            lista.pop(i)
-            lista.pop(i)
-            lista.insert(i, 'XC')
-        elif lista[i] + lista[i + 1] == 'CD':
-            lista.pop(i)
-            lista.pop(i)
-            lista.insert(i, 'CD')
-        elif lista[i] + lista[i + 1] == 'CM':
-            lista.pop(i)
-            lista.pop(i)
-            lista.insert(i, 'CM')
-    romano = lista
+    while True:
+        i += 0
+
+        if i == len(digitado):
+            romano = numero_romano
+            return romano
+        elif digitado[i] == 'I':
+            if i + 1 == len(digitado):
+                numero_romano.append(digitado[i])
+                romano = numero_romano
+                return romano
+            elif digitado[i] + digitado[i + 1] == 'IV':
+                j = 'IV'
+                numero_romano.append(j)
+                i += 2
+            elif digitado[i] + digitado[i + 1] == 'IX':
+                j = 'IX'
+                numero_romano.append(j)
+                i += 2
+            else:
+                numero_romano.append(digitado[i])
+                i += 1
+        elif digitado[i] == 'V':
+            numero_romano.append(digitado[i])
+            i += 1
+        elif digitado[i] == 'X':
+            if i + 1 == len(digitado):
+                numero_romano.append(digitado[i])
+                romano = numero_romano
+                return romano
+            elif digitado[i] + digitado[i + 1] == 'XL':
+                j = 'XL'
+                numero_romano.append(j)
+                i += 2
+            elif digitado[i] + digitado[i + 1] == 'XC':
+                j = 'XC'
+                numero_romano.append(j)
+                i += 2
+            else:
+                numero_romano.append(digitado[i])
+                i += 1
+        elif digitado[i] == 'L':
+            numero_romano.append(digitado[i])
+            i += 1
+        elif digitado[i] == 'C':
+            if i + 1 == len(digitado):
+                numero_romano.append(digitado[i])
+                romano = numero_romano
+                return romano
+            elif digitado[i] + digitado[i + 1] == 'CD':
+                j = 'CD'
+                numero_romano.append(j)
+                i += 2
+            elif digitado[i] + digitado[i + 1] == 'CM':
+                j = 'CM'
+                numero_romano.append(j)
+                i += 2
+            else:
+                numero_romano.append(digitado[i])
+                i += 1
+        elif digitado[i] == 'D':
+            numero_romano.append(digitado[i])
+            i += 1
+        elif digitado[i] == 'M':
+            numero_romano.append(digitado[i])
+            i += 1
+        else:
+            print(f'\nA letra {digitado[i]} não existe!')
+            print("Excluindo para continuar!")
+            i += 1
+
+    romano = numero_romano
     return romano
 
 def verificar(romano):
@@ -40,7 +84,7 @@ def verificar(romano):
     for n, i in enumerate(romano):
         romano_lista.append(i)
     for n, i in enumerate(romano_lista):
-        if romano_lista[n] == 'I' or romano_lista[n] == 'IV' or romano_lista[n] == 'V' or romano_lista[n] == 'X' or romano_lista[n] == 'L' or romano_lista[n] == 'C' or romano_lista[n] == 'D' or romano_lista[n] == 'M':
+        if romano_lista[n] == 'I' or romano_lista[n] == 'IV' or romano_lista[n] == 'V' or romano_lista[n] == 'IX' or romano_lista[n] == 'X' or romano_lista[n] == 'XL' or romano_lista[n] == 'L' or romano_lista[n] == 'XC' or romano_lista[n] == 'C' or romano_lista[n] == 'CD' or romano_lista[n] == 'D' or romano_lista[n] == 'CM' or romano_lista[n] == 'M':
             lista_temporaria.append(romano_lista[n])
         else:
             print(f'\nO "{romano_lista[n]}" não é um número romano!')
@@ -66,14 +110,24 @@ def conversor(romano):
             contador += 4
         elif numero[n] == 'V':
             contador += 5
+        elif numero[n] == 'IX':
+            contador += 9
         elif numero[n] == 'X':
             contador += 10
+        elif numero[n] == 'XL':
+            contador += 40
         elif numero[n] == 'L':
             contador += 50
+        elif numero[n] == 'XC':
+            contador += 90
         elif numero[n] == 'C':
             contador += 100
+        elif numero[n] == 'CD':
+            contador += 400
         elif numero[n] == 'D':
             contador += 500
+        elif numero[n] == 'CM':
+            contador += 900
         elif numero[n] == 'M':
             contador += 1000
         else:
